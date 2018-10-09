135853df97aee35f676672f8ff22cc147aaef80b
Theo Olausson <theoxolausson@gmail.com>
2018-10-09 17:12:00 +0100

    Coin location markers, KDocumentation
    Added functionality for downloading the coin locations
    and then displaying the markers on the MapboxMap.
    Implementation is currently very naive (doesn't cache
    the map for next time, etc).
    
    Also added extensive KDocs documentation to the project.
    
    Finally, removed an unecessary permission request from
    the manifest.

--- Coinz/app/src/main/java/com/coinzgame/theoxo/coinz/MainActivity.kt
+++ Coinz/app/src/main/java/com/coinzgame/theoxo/coinz/MainActivity.kt
@@ -2,7 +2,6 @@ package com.coinzgame.theoxo.coinz
 
 import android.location.Location
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.Menu
@@ -13,7 +12,10 @@ import com.mapbox.android.core.location.LocationEnginePriority
 import com.mapbox.android.core.location.LocationEngineProvider
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.geojson.FeatureCollection
+import com.mapbox.geojson.Point
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.annotations.MarkerOptions
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.MapView
@@ -23,10 +25,14 @@ import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin
 import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode
 import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode
 
-import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
+/**
+ * The app's main Activity, handling loading and displaying the map,
+ * tracking the user's location, and marking the coins on the map.
+ */
 class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineListener,
-        OnMapReadyCallback {
+        OnMapReadyCallback, DownloadCompleteListener {
 
     private lateinit var permissionsManager : PermissionsManager
     private lateinit var originLocation : Location
@@ -38,17 +44,68 @@ class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineLis
 
     private val TAG = "MainActivity"
 
+    /**
+     * First set up method called, getting the Mapbox instance and requesting the MapboxMap
+     *
+     * @param[savedInstanceState] the previously saved instance state, if it exists.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Mapbox.getInstance(this, "***REMOVED***")
+        Mapbox.getInstance(this,
+                "***REMOVED***")
 
         mapView = findViewById(R.id.mapView)
         mapView?.onCreate(savedInstanceState)
         mapView?.getMapAsync(this)
     }
 
+    /**
+     * Listener for the AsyncTask marker map data download having finished.
+     * Calls addMarkers to add the markers to the map.
+     *
+     * @param[result] the downloaded GeoJSON (as a String) which describes the location of the coins
+     */
+    override fun downloadComplete(result: String) {
+        val sneakpeak = result.take(25)
+        Log.d(TAG, "[downloadComplete]: $sneakpeak...")
+        addMarkers(result)
+    }
+
+    /**
+     * Adds the markers for the coins to the MapboxMap being displayed.
+     *
+     * @param[geoJsonString] the downloaded GeoJSON (as a String) which describes the location of the coins
+     */
+    private fun addMarkers(geoJsonString : String) {
+
+        val features = FeatureCollection.fromJson(geoJsonString).features()
+
+        if (features == null) {
+            Log.d(TAG, "[downloadComplete] features is null")
+        } else if (this.mapboxMap == null) {
+            Log.d(TAG, "[downloadComplete] mapboxMap is null, can't add markers")
+        } else {
+            for (feature in features) {
+                val point = feature.geometry() as Point
+                val lat = point.coordinates().get(1)
+                val long = point.coordinates().get(0)
+
+                this.mapboxMap?.addMarker(
+                        MarkerOptions()
+                                .title("Hi")
+                                .position(LatLng(lat, long)))
+            }
+        }
+    }
+
+    /**
+     * Listener function for the async call to receive the MapboxMap. Sets up the local
+     * MapboxMap instance, and then begins to fetch today's coins
+     *
+     * @param[mapboxMap] the received MapboxMap
+     */
     override fun onMapReady(mapboxMap: MapboxMap?) {
         if (mapboxMap == null) {
             Log.d(TAG, "[onMapReady] mapboxMap is null")
@@ -58,8 +115,37 @@ class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineLis
             this.mapboxMap?.uiSettings?.isZoomControlsEnabled = true
 
             enableLocation()
+
+            // Start download from here to make sure that the mapboxMap isn't null when
+            // it's time to add markers
+            val year = Calendar.getInstance().get(Calendar.YEAR).toString()
+            var month = (Calendar.getInstance().get(Calendar.MONTH) + 1).toString()  // Add one as 0-indexed
+            var day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH).toString()
+            if (year != "2018" && year != "2019") {
+                error("Unsupported date")
+            }
+            if (month.length < 2) {
+                // Pad to 0M format
+                month = "0$month"
+            }
+            if (day.length < 2) {
+                // Pad to 0D format
+                day = "0$day"
+            }
+            // TODO make above nicer
+            val date = "$year/$month/$day"
+            Log.d(TAG, date)
+            val date_string = "http://homepages.inf.ed.ac.uk/stg/coinz/" + date + "/coinzmap.geojson"
+            DownloadFileTask(this).execute(
+                    date_string)
         }
     }
+
+    /**
+     * Checks if the necessary location permissions have been granted. If so, invokes the
+     * methods which initialize the LocationEngine and LocationLayerPlugin.
+     * If not, instantiates a PermissionsManager and requests the location permissions.
+     */
     private fun enableLocation() {
         if (PermissionsManager.areLocationPermissionsGranted(this)) {
             Log.d(TAG, "[enableLocation] Permissions granted")
@@ -72,6 +158,9 @@ class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineLis
         }
     }
 
+    /**
+     * Instantiates and sets up the LocationEngine
+     */
     @SuppressWarnings("MissingPermission")
     private fun initializeLocationEngine() {
         locationEngine = LocationEngineProvider(this).obtainBestLocationEngineAvailable()
@@ -91,6 +180,9 @@ class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineLis
         }
     }
 
+    /**
+     * Instantiates and sets up the LocationLayerPlugin
+     */
     @SuppressWarnings("MissingPermission")
     private fun initializeLocationLayer() {
         if (mapView == null) {
@@ -107,6 +199,11 @@ class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineLis
         }
     }
 
+    /**
+     * Updates the current camera position for the displayed MapboxMap
+     *
+     * @param[location] the new Location to focus the camera on
+     */
     private fun setCameraPosition(location : Location) {
         mapboxMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(
                 LatLng(location.latitude, location.longitude), 15.0))
@@ -116,11 +213,16 @@ class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineLis
         // TODO present dialog stating why permissions are needed
     }
 
+    /**
+     * Listener for location listener results. If granted, invokes enableLocation().
+     *
+     * @param[granted] the truth value of the sentence "the permission was granted"
+     */
     override fun onPermissionResult(granted: Boolean) {
         if (granted) {
             enableLocation()
-        }else {
-            print(("Permissions not granted"))
+        } else {
+            Log.d(TAG, "[onPermissionResult] Permissions not granted")
         }
     }
 
@@ -128,22 +230,15 @@ class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineLis
         permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
+    /**
+     * Listener for the user's location changing, updating the recorded and displayed location
+     *
+     * @param[location] the new location found, or null
+     */
     override fun onLocationChanged(location: Location?) {
-        /*if (location == null) {
-            error("This aint workin")
-        } else {
-            location.let {
-                originLocation = location
-                setCameraPosition(location)
-            }
-        }*/
-
         if (location != null) {
             originLocation = location
             setCameraPosition(location)
-
-            // Some demos include the below line, some don't
-            //locationLayerPlugin?.locationEngine?.removeLocationEngineListener(this)
         }
     }
 
@@ -182,6 +277,8 @@ class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineLis
         super.onSaveInstanceState(outState)
         mapView?.onSaveInstanceState(outState)
     }
+
+    /*
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -196,5 +293,6 @@ class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineLis
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
-    }
+    }*/
 }
+
